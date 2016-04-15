7594b43d1803e0eae18ee882f9333948a3a313c3
Bradley Bossard <bradleybossard@gmail.com>
2016-04-15 13:34:24 -0700

    Adding an example using html-webpack-plugin

---
+++ example2/README.md
@@ -0,0 +1,7 @@
+A most basic example of webpack compile
+
+* `npm init --yes` to create blank package.json
+* npm install webpack html-webpack-plugin --save-dev
+* add webpack.config.js
+* add src/index.js with basic console.log statement
+* run `webpack` to compile, now generates an index.html and bundle.js
---
+++ example2/webpack.config.js
@@ -0,0 +1,12 @@
+// webpack.config.js
+var path = require('path')
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: ['./src/index'], // .js after index is optional
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  plugins: [new HtmlWebpackPlugin()]
+}
