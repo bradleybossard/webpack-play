43987eb5b8150546a4a117e14f94bec614530204
Bradley Bossard <bradleybossard@gmail.com>
2016-04-15 17:07:14 -0700

    Adding an example for uglification

---
+++ example4/README.md
@@ -0,0 +1,7 @@
+A example of webpack compile with uglificaiton
+
+* `npm init --yes` to create blank package.json
+* `npm install webpack --save-dev`
+* add webpack.config.js
+* add src/index.js with basic console.log statement
+* run `webpack` to compile
---
+++ example4/webpack.config.js
@@ -0,0 +1,19 @@
+// webpack.config.js
+var path = require('path')
+var webpack = require('webpack')
+//var UglifyJsPlugin = require('html-uglify-plugin');
+
+module.exports = {
+  entry: ['./src/index'], // .js after index is optional
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false,
+      },
+    })
+  ]
+}
